f9c7487938089e03b0a3a5113a8f58e626cc0c33
whalex76 <whalex76@gmail.com>
2015-02-10 09:51:35 +0200

    Transparent zone now closable.

--- src/Imperiv/Bundle/GalleryBundle/Resources/public/js/gallery.js
+++ src/Imperiv/Bundle/GalleryBundle/Resources/public/js/gallery.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
-    $(".transp-zone").on('mouseover', function(event) {
-        $(".transp-zone-close").toggleClass('invisible');
-    })
+    $(".transp-zone-hide").on('click', function(event) {
+        transpZone = $(this).parent().parent();
+        transpZoneContent = transpZone.children(".transp-zone-content");
+        transpZoneControls = $(this).parent();
+        console.log(transpZoneContent);
+        transpZoneControls.children(".transp-zone-hide").toggleClass("invisible");
+        transpZone.animate( { height:"20px" }, { queue:false, duration:500 , complete: function() {
+            transpZoneControls.children(".transp-zone-reveal").toggleClass("invisible");
+            transpZoneContent.toggleClass("invisible");
+        }});
+    });
+    
+    $(".transp-zone-reveal").on('click', function(event) {
+        transpZone = $(this).parent().parent();
+        transpZoneContent = transpZone.children(".transp-zone-content");
+        transpZoneControls = $(this).parent();
+        console.log(transpZoneContent);
+        transpZoneControls.children(".transp-zone-reveal").toggleClass("invisible");
+        transpZone.animate( { height:"100%" }, { queue:false, duration:500 , complete: function() {
+            transpZoneControls.children(".transp-zone-hide").toggleClass("invisible");
+            transpZoneContent.toggleClass("invisible");
+        }});
+    });
 });
