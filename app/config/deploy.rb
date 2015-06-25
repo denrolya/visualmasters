317f85998620e1cf2fd24c4c2a743ec4ddd82ece
drolya <drolya@chemaxon.com>
2015-06-25 22:12:38 +0200

    refactor(deploy): Working on deployment process.

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,8 +1,15 @@
 set :application,                       "imperiumdesign"
 set :domain,                            "46.101.57.96"
-set :user,                              "drolya"
 set :deploy_to,                         "/var/www/#{application}.co.uk"
 set :app_path,                          "app"
+
+set :use_sudo,                          false
+set :user,                              "drolya"
+set :writable_dirs,                     ["app/cache", "app/logs", "app/sessions"]
+set :webserver_user,                    "drolya"
+set :permission_method,                 :acl
+set :use_set_permissions,               true
+
 
 set :clear_controllers,                 false
 
@@ -21,6 +28,11 @@ role :app,                              domain, :primary => true       # This ma
 set :keep_releases,                     5
 
 set :clear_controllers,                 false
+
+set :dump_assetic_assets,               true
+set :use_composer,                      true
+set :shared_files,                      ["app/config/parameters.yml"]
+set :shared_children,                   [app_path + "/logs", web_path + "/uploads", "vendor", app_path + "/sessions"]
 
 logger.level = Logger::MAX_LEVEL
 default_run_options[:pty] = true
