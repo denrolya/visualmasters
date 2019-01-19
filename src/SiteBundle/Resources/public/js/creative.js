043103be3838f40e8b8cc2158ee74f93f7481152
Denis Rolya <denrolya@gmail.com>
2019-01-19 11:23:53 +0200

    fix(scroll): fix scrolling

--- src/SiteBundle/Resources/public/js/creative.js
+++ src/SiteBundle/Resources/public/js/creative.js
@@ -1,7 +1,18 @@
-(function($) {
-    "use strict";
+(function ($) {
+    'use strict';
 
-    $('.page-scroll').on('click', function(event) {
+    setTimeout(function() {
+        coordinates = {
+            about:        $("#about").position().top,
+            services:     $("#services").position().top,
+            portfolio:    $("#portfolio").position().top,
+            media:        $("#media").position().top,
+            contact:      $("#contact").position().top,
+            descriptions: $("#descriptions").position().top
+        };
+    }, 500);
+
+    $('.page-scroll').on('click', function (event) {
         event.preventDefault();
         var position = coordinates[$(this).attr('href').substr(1)];
 
@@ -10,20 +21,20 @@
         }, 500, 'linear');
     });
 
-    $('.page-scroll.scroll-to-tabs').on('click', function(event) {
+    $('.page-scroll.scroll-to-tabs').on('click', function (event) {
         event.preventDefault();
 
         $('html, body').stop().animate({
             scrollTop: coordinates.descriptions
         }, 1000, 'easeInOutExpo');
-    })
+    });
 
     $('body').scrollspy({
         target: '.navbar-fixed-top',
         offset: 51
-    })
+    });
 
-    $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-collapse ul li a').click(function () {
         $('.navbar-toggle:visible').click();
     });
 })(jQuery); // End of use strict
